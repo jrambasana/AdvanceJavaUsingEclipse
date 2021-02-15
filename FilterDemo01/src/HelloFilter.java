8edca9fba1b12a162d301aa9e15898d53733d154
Jatin Ambasana <jatinambasana@gmail.com>
2021-02-15 13:55:23 +0530

    Filter Examples added

---
+++ FilterDemo01/src/HelloFilter.java
@@ -0,0 +1,29 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/hello")
+public class HelloFilter implements Filter {
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		PrintWriter out = response.getWriter();
+		out.println("Filter invoked before.");
+		chain.doFilter(request, response);
+		out.println("Filter invoked after.");
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
---
+++ FilterDemo02/src/WelcomeServlet.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+
+public class WelcomeServlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws ServletException, IOException
+	{
+    		res.setContentType("text/html");
+    		PrintWriter out = res.getWriter();
+    		String username = req.getParameter("username");
+    		out.println("<html><body>Welcome:<b>" + username + "<br/><br/>");
+    		out.println(new Date().toString());
+    		out.println("</b></body></html>");
+	}
+}
