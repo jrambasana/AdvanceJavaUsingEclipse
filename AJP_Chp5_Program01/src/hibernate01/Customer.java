ed762693e3ecd038196028a3b72311a6c750161c
Jatin Ambasana <jatinambasana@gmail.com>
2021-04-30 13:51:37 +0530

    Programs updated

---
+++ AJP_Chp5_Program01/src/hibernate01/Customer.java
@@ -0,0 +1,64 @@
+package hibernate01;
+
+public class Customer {
+    private int id, age;
+    private String first_name, last_name, city;
+    private String contact, address;
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
+}
